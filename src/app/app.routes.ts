5d3e4d416607d62df71a6b9007209c6e034061e7
AmdyMoustafa101 <taphisllefa@gmail.com>
2024-12-20 16:13:28 +0000

    Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,16 @@ import { CohortesComponent } from './components/cohortes/cohortes.component';
 import { EmployeCreateComponent } from './components/Employe/Employe-create/employe-create/employe-create.component';
 import { ApprenantCreateComponent } from './components/Apprenant/Apprenant-create/apprenant-create/apprenant-create.component';
 import { LoginComponent } from './components/login/login/login.component';
+import { AdminPageComponent } from './components/Pages/admin-page/admin-page/admin-page.component';
+import { VigilePageComponent } from './components/Pages/vigile-page/vigile-page/vigile-page.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   {path: 'departement', component: DepartementCreateComponent},
   {path: 'cohorte', component: CohortesComponent},
   {path: 'employe', component: EmployeCreateComponent},
-  {path: 'apprenant', component: ApprenantCreateComponent}
+  {path: 'apprenant', component: ApprenantCreateComponent},
+  {path: 'admin-page', component: AdminPageComponent},
+  {path: 'vigile-page', component: VigilePageComponent}
 
 ];
