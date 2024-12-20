4e7c96c229d2bcad5ce22e6b2b27106a3b3f8cf2
AmdyMoustafa101 <taphisllefa@gmail.com>
2024-12-20 15:37:14 +0000

    Pages

---
+++ src/app/components/Pages/vigile-page/vigile-page/vigile-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vigile-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './vigile-page.component.html',
+  styleUrl: './vigile-page.component.css'
+})
+export class VigilePageComponent {
+
+}
